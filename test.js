683b4b35ffe46f7be47f5e6e92ca5f4beb792483
Frijol <ifoundthemeaningoflife@gmail.com>
2014-07-19 14:16:12 -0700

    updated test code to work with release version of API

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 var tessel = require('tessel');
-var gpio = tessel.port('gpio');
-var ch1 = gpio.analog[2];
-var ch2 = gpio.analog[6];
+var gpio = tessel.port['GPIO'];
+var ch1 = gpio.analog[1];
+var ch2 = gpio.analog[5];
 
 setInterval(function() {
-  console.log('ch 1', ch1.read(), 'ch 2', ch2.read());
+  console.log('ch 1', ch1.read(), '\tch 2', ch2.read());
 }, 50);
